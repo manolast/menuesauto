d13ae309fffd6ce5eb3d9d490ae2bd7ac3279946
manolast <manuel.stapff@gmail.com>
2023-03-07 12:44:35 -0300

    commit semi final

--- automatizadorForms/main.py
+++ automatizadorForms/main.py
@@ -1,5 +1,6 @@
 import time
 import csv
+import pwinput
 
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
@@ -9,9 +10,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
-
-
-
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 
 options = Options();
 options.add_experimental_option("detach", True)
@@ -20,71 +19,65 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 
 driver.get("https://login.ordereat.com.uy/generate_registration")
 driver.maximize_window()
-time.sleep(4)
+driver.implicitly_wait(5)
 maillogin = driver.find_element(By.XPATH, '//*[@id="normal_login_username"]')
 passlogin = driver.find_element(By.XPATH, '//*[@id="normal_login_password"]')
 
-maillogin.send_keys(input('email:'))
-passlogin.send_keys(input('contrasena:'))
-
-time.sleep(5)
-
-buttonlogin = driver.find_element(By.XPATH, '//*[@id="normal_login"]/div[3]/div/div/div/button')
-buttonlogin.click()
-
-time.sleep(10)
+maillogin.send_keys(input('Email: '))
+passlogin.send_keys(pwinput.pwinput(prompt='Contraseña: '))
+passlogin.send_keys(Keys.ENTER)
 
 nombre_pais = input('De que pais es la cantina: ')
 nombre_cantina = input('Nombre de la cantina: ')
-
+csv_file = input('Nombre del Archivo: ')
 
 dropdown_pais = driver.find_element(By.XPATH, '/html/body/div[1]/div/section/div/div/div/div/div/div/div[1]/div/div/span[1]/input')
 dropdown_pais.send_keys(nombre_pais)
 dropdown_pais.send_keys(Keys.ENTER)
 
-time.sleep(10)
+time.sleep(3)
 
-
-# try:
-tr_cantina = driver.find_element(By.XPATH, f"//td[contains(text(),'{nombre_cantina}')]/ancestor::tr")
-
-    #boton iniciar sesion
-boton_sesion = driver.find_element(By.XPATH, f"//td[contains(text(),'{nombre_cantina}')]/ancestor::tr/td/div/button[5]")
-boton_sesion.click()
+cantina_found = False
+while not cantina_found:
+    # Buscar boton cantina
+    try:
+        button_sesion = driver.find_element(By.XPATH, f"//td[contains(text(),'{nombre_cantina}')]/ancestor::tr/td/div/button[5]")
+        button_sesion.click()
+        cantina_found = True
+    except NoSuchElementException:
+        try:
+            # Si no se encuentra el boton de inicio de sesion para la cantina
+            button_nextpage = driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div/div/div/div/div/ul/li[7]/button')
+            button_nextpage.click()
+            # Esperar que cargue pagina
+            driver.implicitly_wait(5)
+        except:
+            # Si tiene error al clickear boton next page, se llego a la ultima pagina y no se encontro la cantina
+            print('No se pudo encontrar la cantina. Checkea los datos de país y nombre de la cantina')
+            break
 
 hamb_button = driver.find_element(By.XPATH, '//*[@id="root"]/header/button')
 hamb_button.click()
-boton_menus = driver.find_element(By.XPATH, '//a[@href="/menus"]')
-boton_menus.click()
-time.sleep(3)
-# except: #NoSuchElementException:
-#     # If the TD element is not found, click the button that takes to the next page
-#     next_page_button = driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div/div/div/div/div/ul/li[7]/button')
-#     next_page_button.click()
+button_menus = driver.find_element(By.XPATH, '//a[@href="/menus"]')
+button_menus.click()
+driver.implicitly_wait(5)
 
-# with open('csvprueba.csv', 'r') as archivo_csv:
-#     lector_csv = csv.DictReader(archivo_csv)
-#     for fila in lector_csv:
-#         buscar = driver.find_element(By.XPATH, "//input[@class='gLFyf']")
-#         buscar.send_keys(fila['email'])
-#         time.sleep(5)
-#         buscar.send_keys(Keys.ENTER)
-#         time.sleep(5)
-
-
-nuevo_menu = driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div/button')
-nuevo_menu.click()
-input_titulo = driver.find_element(By.XPATH, '//*[@id="name"]')
-input_descrip = driver.find_element(By.XPATH, '//*[@id="description"]')
-input_fecha = driver.find_element(By.XPATH, '//*[@id="displayDate"]')
-input_precio = driver.find_element(By.XPATH, '//*[@id="price"]')
-
-input_titulo.send_keys('sandwich')
-input_descrip.send_keys('de jamon y queso')
-input_fecha.send_keys('2023-05-07')
-input_precio.send_keys(500)
-
-time.sleep(10)
-
-guardar_menu = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div/div[2]/div/div/form/div[2]/div[5]/div[2]/button[2]')
-guardar_menu.click()
+with open(f"{csv_file}", 'r') as archivo_csv:
+    lector_csv = csv.DictReader(archivo_csv)
+    for fila in lector_csv:
+        #clickea para crear nuevo menu
+        nuevo_menu = driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div/button')
+        nuevo_menu.click()
+        #identifica inputs
+        input_titulo = driver.find_element(By.XPATH, '//*[@id="name"]')
+        input_descrip = driver.find_element(By.XPATH, '//*[@id="description"]')
+        input_fecha = driver.find_element(By.XPATH, '//*[@id="displayDate"]')
+        input_precio = driver.find_element(By.XPATH, '//*[@id="price"]')
+        #llena inputs con los datos del csv
+        input_titulo.send_keys(fila['titulo'])
+        input_descrip.send_keys(fila['descripcion'])
+        input_fecha.send_keys(fila['fecha'])
+        input_precio.send_keys(fila['precio'])
+        time.sleep(2)
+        #guarda el menu apretando enter
+        input_precio.send_keys(Keys.ENTER)
