5d4b74920430205057e4ab4413fee4e78ddab310
manolast <manuel.stapff@gmail.com>
2023-03-06 21:59:48 -0300

    commit sape

--- automatizadorForms/main.py
+++ automatizadorForms/main.py
@@ -7,6 +7,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
 
 
 options = Options();
@@ -27,14 +29,23 @@ dropdown_pais.click
 selector_pais = driver.find_element(By.XPATH, f"//div[@title={nombre_pais} and ancestor::div[4][preceding-sibling::div[@id='rc_select_4_list']]]")
 selector_pais.click
 
-div_cantina = 
-if ()
 
-with open('csvprueba.csv', 'r') as archivo_csv:
-    lector_csv = csv.DictReader(archivo_csv)
-    for fila in lector_csv:
-        buscar = driver.find_element(By.XPATH, "//input[@class='gLFyf']")
-        buscar.send_keys(fila['email'])
-        time.sleep(5)
-        buscar.send_keys(Keys.ENTER)
-        time.sleep(5)
+try:
+    tr_cantina = driver.find_element(By.XPATH, f"//td[contains(text(),'{nombre_cantina}')]/ancestor::tr")
+
+    #boton iniciar sesion
+    boton_sesion = tr_cantina.find_element(By.XPATH, './td//button[descendant::span[contains(@class, 'anticon-login')]]')
+    boton_sesion.click()
+except: #NoSuchElementException:
+    # If the TD element is not found, click the button that takes to the next page
+    next_page_button = driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div/div/div/div/div/ul/li[7]/button')
+    next_page_button.click()
+
+# with open('csvprueba.csv', 'r') as archivo_csv:
+#     lector_csv = csv.DictReader(archivo_csv)
+#     for fila in lector_csv:
+#         buscar = driver.find_element(By.XPATH, "//input[@class='gLFyf']")
+#         buscar.send_keys(fila['email'])
+#         time.sleep(5)
+#         buscar.send_keys(Keys.ENTER)
+#         time.sleep(5)
