7f6b2c597c2e6803ad2f467ec4995dc6042f11a5
manolast <103449499+manolast@users.noreply.github.com>
2023-03-06 23:58:08 -0300

    Update main.py

--- automatizadorForms/main.py
+++ automatizadorForms/main.py
@@ -8,6 +8,8 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+
 
 
 
@@ -19,27 +21,46 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 driver.get("https://login.ordereat.com.uy/generate_registration")
 driver.maximize_window()
 time.sleep(4)
+maillogin = driver.find_element(By.XPATH, '//*[@id="normal_login_username"]')
+passlogin = driver.find_element(By.XPATH, '//*[@id="normal_login_password"]')
 
-nombre_pais = input('De que pais es la cantina')
-nombre_cantina = input('Nombre de la cantina')
+maillogin.send_keys(input('email:'))
+passlogin.send_keys(input('contrasena:'))
 
-#encontrar y clickear pais
-dropdown_pais = driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div/div/div/div/div/div/div[1]/div')
-dropdown_pais.click
-selector_pais = driver.find_element(By.XPATH, f"//div[@title={nombre_pais} and ancestor::div[4][preceding-sibling::div[@id='rc_select_4_list']]]")
-selector_pais.click
+time.sleep(5)
+
+buttonlogin = driver.find_element(By.XPATH, '//*[@id="normal_login"]/div[3]/div/div/div/button')
+buttonlogin.click()
+
+time.sleep(10)
+
+nombre_pais = input('De que pais es la cantina: ')
+nombre_cantina = input('Nombre de la cantina: ')
 
 
-try:
-    tr_cantina = driver.find_element(By.XPATH, f"//td[contains(text(),'{nombre_cantina}')]/ancestor::tr")
+dropdown_pais = driver.find_element(By.XPATH, '/html/body/div[1]/div/section/div/div/div/div/div/div/div[1]/div/div/span[1]/input')
+dropdown_pais.send_keys(nombre_pais)
+dropdown_pais.send_keys(Keys.ENTER)
+
+time.sleep(10)
+
+
+# try:
+tr_cantina = driver.find_element(By.XPATH, f"//td[contains(text(),'{nombre_cantina}')]/ancestor::tr")
 
     #boton iniciar sesion
-    boton_sesion = tr_cantina.find_element(By.XPATH, './td//button[descendant::span[contains(@class, 'anticon-login')]]')
-    boton_sesion.click()
-except: #NoSuchElementException:
-    # If the TD element is not found, click the button that takes to the next page
-    next_page_button = driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div/div/div/div/div/ul/li[7]/button')
-    next_page_button.click()
+boton_sesion = driver.find_element(By.XPATH, f"//td[contains(text(),'{nombre_cantina}')]/ancestor::tr/td/div/button[5]")
+boton_sesion.click()
+
+hamb_button = driver.find_element(By.XPATH, '//*[@id="root"]/header/button')
+hamb_button.click()
+boton_menus = driver.find_element(By.XPATH, '//a[@href="/menus"]')
+boton_menus.click()
+time.sleep(3)
+# except: #NoSuchElementException:
+#     # If the TD element is not found, click the button that takes to the next page
+#     next_page_button = driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div/div/div/div/div/ul/li[7]/button')
+#     next_page_button.click()
 
 # with open('csvprueba.csv', 'r') as archivo_csv:
 #     lector_csv = csv.DictReader(archivo_csv)
@@ -49,3 +70,21 @@ except: #NoSuchElementException:
 #         time.sleep(5)
 #         buscar.send_keys(Keys.ENTER)
 #         time.sleep(5)
+
+
+nuevo_menu = driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div/button')
+nuevo_menu.click()
+input_titulo = driver.find_element(By.XPATH, '//*[@id="name"]')
+input_descrip = driver.find_element(By.XPATH, '//*[@id="description"]')
+input_fecha = driver.find_element(By.XPATH, '//*[@id="displayDate"]')
+input_precio = driver.find_element(By.XPATH, '//*[@id="price"]')
+
+input_titulo.send_keys('sandwich')
+input_descrip.send_keys('de jamon y queso')
+input_fecha.send_keys('2023-05-07')
+input_precio.send_keys(500)
+
+time.sleep(10)
+
+guardar_menu = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div/div[2]/div/div/form/div[2]/div[5]/div[2]/button[2]')
+guardar_menu.click()
